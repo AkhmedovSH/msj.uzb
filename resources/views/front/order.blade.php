866b357330ceeaec4e8ed28c723a7691a08db237
Shokhrukh <shurikaxmedov1@gmail.com>
2021-08-11 10:58:56 +0500

    fixed

--- resources/views/front/order.blade.php
+++ resources/views/front/order.blade.php
@@ -1,22 +1,6 @@
 @extends('layout')
 @section('content')
-<script>
-	function selectCity() {
-		var city = document.getElementById("city").value;
-		switch (city) {
-			case 'Самарканд':
-				document.getElementById("delivery_price").innerHTML = '40 000'
-				document.getElementById("total_price").innerHTML = '40 000'
-				break;
-			case 'Бухара':
-				document.getElementById("delivery_price").innerHTML = '40 000'
-				break;
-			case 'Ташкент':
-				document.getElementById("delivery_price").innerHTML = '25 000'
-				break;
-		}
-	}
-</script>
+
 <main>
 	<section class="ordering">
 		<div class="container-fluid">
@@ -43,7 +27,10 @@
 										<span id="delivery_price" class="delivery-price">25 000</span>
 										<span class="delivery-price"> сум</span>
 									</p>
-									<p class="form__select-text">Стоимость товара: <span class="product-price">{{ Cart::subtotal(0, 0, ' ') }} сум</span></p>
+									<p class="form__select-text">
+										<input id="products_price" type="text" class="d-none" value="{{ Cart::subtotal(0, 0, '') }}">
+										Стоимость товара: <span class="product-price">{{ Cart::subtotal(0, 0, ' ') }} сум</span>
+									</p>
 									<p class="form__select-text">
 										Общая стоимость: 
 										<span id="total_price" class="total-price">{{ Cart::subtotal(0, 0, ' ') }}</span>
@@ -54,7 +41,7 @@
 							<div class="form__radio-group">
 								Выберите способ оплаты:
 								<div class="form__label-group">
-									<label class="form__label">
+									<label class="form__label" style="margin-bottom: 0;">
 										<input type="radio" name="payment_type" value="cash" class="form__radio styler" checked>
 										Наличные
 									</label>
@@ -62,10 +49,10 @@
 										<input type="radio" name="payment_type" value="click" class="form__radio styler">
 										<img src="/assets/icons/click-logo.svg" alt="Click Logo">
 									</label>
-									<label class="form__label">
+									<!-- <label class="form__label">
 										<input type="radio" name="payment_type" value="payme" class="form__radio styler">
 										<img src="/assets/icons/payme-logo.svg" alt="Payme Logo">
-									</label>
+									</label> -->
 								</div>
 							</div>
 						</div>
@@ -82,5 +69,48 @@
 		</div>
 	</section>
 </main>
+<script>
+	var city = document.getElementById("city").value;
+	var price = document.getElementById("products_price").value;
+	price = Number(price) + 25000
+	document.getElementById("total_price").innerHTML = formatMoney(price)
 
+	function selectCity() {
+		var city = document.getElementById("city").value;
+		var price = document.getElementById("products_price").value;
+		switch (city) {
+			case 'Самарканд':
+				document.getElementById("delivery_price").innerHTML = '40 000'
+				price = Number(price) + 40000
+				document.getElementById("total_price").innerHTML = formatMoney(price)
+				break;
+			case 'Бухара':
+				document.getElementById("delivery_price").innerHTML = '40 000'
+				price = Number(price) + 40000
+				document.getElementById("total_price").innerHTML = formatMoney(price)
+				break;
+			case 'Ташкент':
+				document.getElementById("delivery_price").innerHTML = '25 000'
+				price = Number(price) + 25000
+				document.getElementById("total_price").innerHTML = formatMoney(price)
+				break;
+		}
+	}
+
+	function formatMoney(amount, decimalCount = 0, decimal = ".", thousands = " ") {
+		try {
+			decimalCount = Math.abs(decimalCount);
+			decimalCount = isNaN(decimalCount) ? 0 : decimalCount; // 0 was 2
+	
+			const negativeSign = amount < 0 ? "-" : "";
+	
+			let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+			let j = (i.length > 3) ? i.length % 3 : 0;
+	
+			return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+		} catch (e) {
+			console.log(e)
+		}
+	}
+</script>
 @endsection
